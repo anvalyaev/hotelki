3bdc533b33d77f3612b4ce15ceb32521fbc57ac5
Andrey Valyaev <an.valyaev@yandex.ru>
2019-01-03 21:02:31 +0300

    small fix

--- hotclient/lib/model/index.dart
+++ hotclient/lib/model/index.dart
@@ -2,7 +2,7 @@ export 'model.dart';
 export 'wishlist.dart';
 export 'notification.dart';
 export 'action.dart';
-export 'implmodel/authorize.dart';
+export 'implmodel/account.dart';
 export 'implmodel/network.dart';
 export 'implnetworkrequest/initauthorize.dart';
 export 'implnetworkrequest/initregister.dart';
